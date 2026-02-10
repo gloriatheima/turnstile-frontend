6c98aa8418762f0f998b26ad05f78e7f6d1b8bcb
gloria1234 <caofang2013@yeah.net>
2026-02-10 18:07:08 +0800

    删除eid在localstorage中的存储

--- src/App.vue
+++ src/App.vue
@@ -72,18 +72,14 @@ export default {
           headers: {
             "Content-Type": "application/json",
             "x-turnstile-token": this.token, // 新增这一行主动添加一个request header头 || 企业版 WAF可以写 body 匹配规则，表达式如 (http.request.body contains "token")
-
+            // 在 Worker 返回的 headers 中加入
           },
           body,
         });
         const data = await res.json();
         if (data.success) {
           this.verifyMsg = "Turnstile 验证通过！";
-          const eid = data.ephemeral_id
-          if (eid){
-           localStorage.setItem('cf_eid', eid); 
-        console.log("设备 ID 已记录，后续请求将受到保护");
-          }
+          
         } else {
           // this.verifyMsg = "验证失败：" + (data.errors || "未知错误");
           this.verifyMsg =
